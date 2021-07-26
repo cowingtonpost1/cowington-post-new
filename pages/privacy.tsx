a27f46b4d0754c0bb732b181c03a31c5236872f0
scienceboy1203 <51143004+scienceboy1203@users.noreply.github.com>
2021-07-26 05:50:58 -0700

    Fixed Boring Privacy policy stuff

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -15,7 +15,7 @@ function PrivacyPolicy() {
                 We use Your Personal data to provide and improve the Service. By
                 using the Service, You agree to the collection and use of
                 information in accordance with this Privacy Policy. This Privacy
-                Policy has been created with the help of the{' '}
+                Policy has been created with the help of the
                 <a
                     href="https://www.termsfeed.com/privacy-policy-generator/"
                     target="_blank"
