e10adf1f530e70a0ffeaf997217cfc3413abd519
scienceboy1203 <51143004+scienceboy1203@users.noreply.github.com>
2021-07-26 16:56:34 -0700

    Fixed Topic Links

--- Components/NewTopicCard4.tsx
+++ Components/NewTopicCard4.tsx
@@ -11,60 +11,61 @@ import Link from 'next/link'
 
 export default function ProductSimple(props) {
     return (
-        <Link href={props.href}>
+        <Link href={props.href} passHref>
+            <Box
+                role={'group'}
+                p={6}
+                maxW={'330px'}
+                w={'full'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'2xl'}
+                rounded={'lg'}
+                pos={'relative'}
+                zIndex={1}
+                _bg={useColorModeValue('gray.100', 'gray.600')}
+            >
                 <Box
-                    role={'group'}
-                    p={6}
-                    maxW={'330px'}
-                    w={'full'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
                     rounded={'lg'}
+                    mt={-12}
                     pos={'relative'}
-                    zIndex={1}
-					_bg={useColorModeValue('gray.100', 'gray.600')}
+                    height={'230px'}
+                    _after={{
+                        transition: 'all .3s ease',
+                        content: '""',
+                        w: 'full',
+                        h: 'full',
+                        pos: 'absolute',
+                        top: 5,
+                        left: 0,
+                        backgroundImage: `url(${props.image})`,
+                        filter: 'blur(15px)',
+                        zIndex: -1,
+                    }}
+                    _groupHover={{
+                        _after: {
+                            filter: 'blur(20px)',
+                        },
+                    }}
                 >
-                    <Box
+                    <Image
                         rounded={'lg'}
-                        mt={-12}
-                        pos={'relative'}
-                        height={'230px'}
-                        _after={{
-                            transition: 'all .3s ease',
-                            content: '""',
-                            w: 'full',
-                            h: 'full',
-                            pos: 'absolute',
-                            top: 5,
-                            left: 0,
-                            backgroundImage: `url(${props.image})`,
-                            filter: 'blur(15px)',
-                            zIndex: -1,
-                        }}
-                        _groupHover={{
-                            _after: {
-                                filter: 'blur(20px)',
-                            },
-                        }}
-                    >
-                        <Image
-                            rounded={'lg'}
-                            height={230}
-                            width={282}
-                            objectFit={'cover'}
-                            src={props.image}
-                        />
-                    </Box>
-                    <Stack pt={10} align={'center'}>
-                        <Heading
-                            fontSize={'3xl'}
-                            fontFamily={'body'}
-                            fontWeight={500}
-                        >
-                            {props.title}
-                        </Heading>
-                    </Stack>
+                        height={230}
+                        width={282}
+                        objectFit={'cover'}
+                        alt="Image"
+                        src={props.image}
+                    />
                 </Box>
+                <Stack pt={10} align={'center'}>
+                    <Heading
+                        fontSize={'3xl'}
+                        fontFamily={'body'}
+                        fontWeight={500}
+                    >
+                        {props.title}
+                    </Heading>
+                </Stack>
+            </Box>
         </Link>
     )
 }
